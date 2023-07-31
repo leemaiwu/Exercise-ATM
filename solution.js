2a9efa50a818c3e3e45be32f4d6ad7ddad8528d3
Laura Wu <leemaiwu@gmail.com>
2023-07-30 19:58:41 -0600

    Initial commit

---
+++ solution.js
@@ -0,0 +1,12 @@
+const solve = (n) => {
+    let transaction = 0
+    if (n % 10 !== 0) {
+        return -1
+    } 
+     return 1
+}
+
+console.log(solve(125)) // -1
+console.log(solve(1500)) // 1
+console.log(solve(770)) // 4
+console.log(solve(10)) // 1
